a4470619f2e239ac9e4da374e7e73cba1c1bdd74
Pablo Hernandez <hpablo575@gmail.com>
2022-03-08 20:31:33 -0700

    debugged js and css files. Still having issues with the character div as it wont stop jumping

--- fetch.js
+++ fetch.js
@@ -1,6 +1,24 @@
 var character =
 document.getElementById("character"); 
 var block = document.getElementById("block");
-function jump () {
-    character.classList.add("animate")
+
+function jump() {
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    }
 }
+var checkDead = setInterval(function(){
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = 
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        block.style.display = "none"
+        alert("u loose lol")
+
+    }
+    }, 10);
